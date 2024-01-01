6eec1a310867ea2c480a699cd44e79d473d13ed0
iman <dev.imansabet@gmail.com>
2024-01-01 14:44:22 +0330

    Add Repository Interfaces

---
+++ CRUD_API/Repository/IAuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using CRUD_API.Model;
+
+namespace CRUD_API.Repository
+{
+    public interface IAuthorRepository
+    {
+        Task<IEnumerable<Author>> GetAuthorsAsync(int page, int pageSize, string search);
+        Task<Author> GetAuthorAsync(int id);
+        Task AddAuthorAsync(Author author);
+        Task UpdateAuthorAsync(Author author);
+        Task DeleteAuthorAsync(int id);
+    }
+}
---
+++ CRUD_API/Repository/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using CRUD_API.Model;
+
+namespace CRUD_API.Repository
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetBooksAsync(int page, int pageSize, string search);
+        Task<Book> GetBookAsync(int id);
+        Task AddBookAsync(Book book);
+        Task UpdateBookAsync(Book book);
+        Task DeleteBookAsync(int id);
+    }
+}
