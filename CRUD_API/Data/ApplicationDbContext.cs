a27eeb2f11a0e46fe23d46e7d2fe5cbdcc873341
iman <dev.imansabet@gmail.com>
2024-01-01 13:38:19 +0330

    Add Models & UpdateDB

--- CRUD_API/Data/ApplicationDbContext.cs
+++ CRUD_API/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CRUD_API.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUD_API.Data
 {
@@ -8,7 +9,8 @@ namespace CRUD_API.Data
         {
             
         }
-
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
     }
 
 }
