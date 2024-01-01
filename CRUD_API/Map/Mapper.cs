3f4b80e7c15b9ab5af6912793e9cd6f7120442f5
iman <dev.imansabet@gmail.com>
2024-01-01 14:37:02 +0330

    Add Mapper

---
+++ CRUD_API/Map/Mapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CRUD_API.Model;
+
+namespace CRUD_API.Map
+{
+    public class Mapper : Profile
+    {
+        public Mapper() 
+        {
+            CreateMap<Author, AuthorDTO>().ReverseMap();
+            CreateMap<Book, BookDTO>().ReverseMap();
+        }
+    }
+}
