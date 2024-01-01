8d645b28a862a90f37f90b7af0c7c77b77baaa68
iman <dev.imansabet@gmail.com>
2024-01-01 14:59:44 +0330

    Implement Repositories

---
+++ CRUD_API/Repository/AuthorRepository.cs
@@ -0,0 +1,61 @@
+﻿using CRUD_API.Data;
+using CRUD_API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_API.Repository
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Author>> GetAuthorsAsync(int page, int pageSize, string search)
+        {
+            var query = _context.Authors.AsQueryable();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(a => a.Name.Contains(search));
+            }
+
+            var authors = await query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return authors;
+        }
+
+        public async Task<Author> GetAuthorAsync(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+            return author;
+        }
+
+        public async Task AddAuthorAsync(Author author)
+        {
+            _context.Authors.Add(author);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAuthorAsync(Author author)
+        {
+            _context.Entry(author).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAuthorAsync(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+            if (author != null)
+            {
+                _context.Authors.Remove(author);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ CRUD_API/Repository/BookRepository.cs
@@ -0,0 +1,63 @@
+﻿using CRUD_API.Data;
+using CRUD_API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_API.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksAsync(int page, int pageSize, string search)
+        {
+            var query = _context.Books
+                .Include(b => b.Author)
+                .AsQueryable();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(b => b.Title.Contains(search) || b.Author.Name.Contains(search));
+            }
+
+            var books = await query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return books;
+        }
+
+        public async Task<Book> GetBookAsync(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            return book;
+        }
+
+        public async Task AddBookAsync(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBookAsync(Book book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBookAsync(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
